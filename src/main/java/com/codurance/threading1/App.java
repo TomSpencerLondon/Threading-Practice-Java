432df27738ddb7b87945ba983a0593be40f5765d
Tom <thomas.spencer@codurance.com>
2021-05-09 12:31:07 +0100

    Add code for threading practice

---
+++ src/main/java/com/codurance/threading1/App.java
@@ -0,0 +1,23 @@
+package com.codurance.threading1;
+
+class Runner extends Thread {
+  public void run() {
+    for (int i = 0; i < 10; i++){
+      System.out.println("Hello " + i);
+
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+
+public class App {
+
+  public static void main(String[] args) {
+
+  }
+}
