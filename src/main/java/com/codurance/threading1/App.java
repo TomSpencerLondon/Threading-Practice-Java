81529217ab48b0c6952ee27714678527d92eca46
Tom <thomas.spencer@codurance.com>
2021-05-09 12:33:01 +0100

    Concurrent code running

--- src/main/java/com/codurance/threading1/App.java
+++ src/main/java/com/codurance/threading1/App.java
@@ -18,6 +18,10 @@ class Runner extends Thread {
 public class App {
 
   public static void main(String[] args) {
+    Runner runner1 = new Runner();
+    runner1.start();
 
+    Runner runner2 = new Runner();
+    runner2.start();
   }
 }
